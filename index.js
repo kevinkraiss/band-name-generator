4a18fe329ea7b1d7b9282dc14c93d4eb61b61371
Kevin Kraiss <kevinkraiss@gmail.com>
2022-10-24 21:22:14 -0500

    adds js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('test')
+import chalkAnimation from 'chalk-animation'
+
+import { generateSlug } from 'random-word-slugs'
+
+
+const length = +process.argv[2]
+
+const bandName = generateSlug(length, {format: 'title'})
+
+chalkAnimation.pulse(bandName)
